2986ba8b85d19d8e90981f46a8c87cb144c3c82f
Abel Lucas <abelucas009@gmail.com>
2023-03-08 10:40:48 -0300

    new

---
+++ ILovePython.py
@@ -0,0 +1,44 @@
+###################
+# ILovePython App #
+###################
+#Importações necessárias
+from customtkinter import CTk,CTkLabel,CTkImage,CTkButton,set_appearance_mode
+from os.path import dirname
+from random import randint
+from PIL import Image
+
+#Janela Padronizada
+class Master(CTk):
+    def __init__(self, width= int, height= int, title= str, resizable= bool):
+        super().__init__()
+        self.minsize(width,height)
+        self.title(title)
+        self.resizable(resizable,resizable)
+        self.iconbitmap(dirname(__file__) +"\\resources/icon.ico")
+        set_appearance_mode("Light")
+
+#Elementos da interface
+class App:
+    def __init__(self, master):
+        master.columnconfigure(3, weight=3)
+        master.rowconfigure(6, weight=6)
+        
+        self.lbl=CTkLabel(master, text="Você ama programar em python?")
+        self.lbl.grid(row=1, column=2, columnspan=3, pady=5)
+        
+        img=CTkImage(Image.open(dirname(__file__) +"\\resources/py.png"))
+        image=CTkLabel(master,image= img, text=None)
+        image.grid(row=2, column=2, columnspan=3, pady=5)
+        
+        self.btnYes=CTkButton(master, text="Sim", command= master.destroy)
+        self.btnNo=CTkButton(master, text="Não", command= lambda :self.btnNo.grid(row=4, column=randint(2,4), pady=5))
+        self.btnYes.grid(row=3, column=2, columnspan=3, pady=5)
+        self.btnNo.grid(row=4, column=3, pady=5)
+
+##########################################
+#Verificação de execução direta do módulo#
+##########################################
+if __name__ == "__main__":
+    master= Master(400,200,"ILovePython", False)
+    app= App(master)
+    master.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# ILovePython
- 
+<div align="center">
+<div>
+<img src="img/ILovePython icon.png" alt="ILovePython icon">
+</div>
+
+<div>
+  <h4></h4>
+  <figure>
+    <img src="img/interface.png" alt="Interface do ILovePython"></br>
+    <figcaption>Aba/Interface do ILovePython</figcaption>
+  </figure>
+</div>
+
+<div>
+  <h3>Sobre o projeto</h3>
+  <p>Criado como uma resenha para brincar com quem não ama a linguagem Python, o ILovePython é um aplicativo com interface simples com objetivo de testar meus conhecimentos em python utilizando o módulo CustomTkinter e brincando com seu sistema de grids, rows e columns.</p>
+  <p>
+    <a href="https://www.python.org/">
+      <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" alt="Python V.3.9" ></a>
+    <a href="https://github.com/TomSchimansky/CustomTkinter">
+      <img src="https://img.shields.io/badge/Customtkinter-V.5.1.2-blue?style=for-the-badge&logo=python&logoColor=white" alt="Customtkinter V.5.1.2" ></a>
+  </p>
+
+<div>
+  <h3>Resultado</h3>
+  <img src="img/ILovePython.gif" alt="ILovePython gif">
+</div>
+</div>
+</div>
